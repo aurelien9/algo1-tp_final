62ac936984c3656c13921e1a0a0a1db2abbe283e
aurelien9 <aure.flout@hotmail.fr>
2017-11-16 17:42:22 -0300

    create struct

---
+++ struct.h
@@ -0,0 +1,41 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#define MAX_LENGTH_MENSAJE 140
+
+
+typedef struct
+{
+  int num;
+  t_date stamp;
+  char mensaje[MAX_LENGTH_MENSAJE];
+  int id;
+}mensaje;
+
+
+typedef struct lista_s lista_s;
+
+struct lista_s
+{
+  lista_s *sig;
+  mensaje *datos;
+};
+
+
+typedef struct
+{
+  int real;
+  int alloc;
+  int *datos;
+}vector_s;
+
+
+typedef struct usuario {
+  int id ;
+  t_cadena nombre;
+  t_cadena usuario;
+  vector_s amigos;
+  lista_s mensajes;
+}usuario;
+
+#endif
