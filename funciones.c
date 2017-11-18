02121ec16fdda8358adf90213679bf316fa7046b
aurelien9 <aure.flout@hotmail.fr>
2017-11-18 16:09:04 -0300

    aa

--- funciones.c
+++ funciones.c
@@ -3,12 +3,55 @@
 
 #include "struct.h"
 
-
+#define MAX_LENGTH 140
 
 status_t cargar_usuarios(lista_usuarios *v, FILE *pf)
 {
 
-	while(fgets())
+	char renglon[MAX_LENGTH] = "";
 
-		return ST_OK;
+	while(fgets(renglon, MAX_LENGTH, pf) != NULL)
+	{
+		if(renglon[1] == '[')
+		{
+			cargar_usuario(v, renglon, pf);
+		}
+	}
+
+	return ST_OK;
+}
+
+
+
+status_t cargar_usuario(lista_usuarios v, char* renglon, FILE *pf)
+{
+	lista_usuarios *actual = v;
+	int id;
+
+	while(actual != NULL)
+		actual = actual->sig;
+
+	/* creacion del usuario*/
+	actual = (lista_usuarios*)malloc(sizeof(lista_usuarios));
+
+	actual.sig = NULL;
+	actual.datos = (usuario*)malloc(sizeof(usuario));
+
+	/* completar el usuario*/
+	strncpy(actual->datos->usuario, renglon);
+
+	fgets(renglon, MAX_LENGTH, pf);
+
+	actual->datos->id = convert_renglon_id(renglon); /*a hacer*/
+
+	fgets(renglon, MAX_LENGTH, pf);
+
+	actual->datos->nombre = convert_renglon_nombre(); /*a hacer*/
+
+	fgets(renglon, MAX_LENGTH, pf);
+
+	actual->datos->amigos = crear_vector_amigos(renglon); /*a hacer*/
+
+	crear_lista_mensaje(actual->datos); /*a hacer*/
+
 }
--- struct.h
+++ struct.h
@@ -3,19 +3,21 @@
 
 #define MAX_LENGTH_MENSAJE 140
 
+typedef char* t_cadena;
+
 typedef struct
 {
-  unsigned int year;
-  unsigned int month;
-  unsigned int day;
+	unsigned int year;
+	unsigned int month;
+	unsigned int day;
 }
 
 typedef struct
 {
-  int num;
-  t_date stamp;
-  char mensaje[MAX_LENGTH_MENSAJE];
-  int id;
+	int num;
+	t_date stamp;
+	char mensaje[MAX_LENGTH_MENSAJE];
+	int id;
 }mensaje;
 
 
@@ -23,25 +25,26 @@ typedef struct lista_s lista_s;
 
 struct lista_s
 {
-  lista_s *sig;
-  mensaje *datos;
+	lista_s *sig;
+	mensaje *datos;
 };
 
 
 typedef struct
 {
-  int real;
-  int alloc;
-  int *datos;
+	int real;
+	int alloc;
+	int *datos;
 }vector_s;
 
 
-typedef struct usuario {
-  int id ;
-  t_cadena nombre;
-  t_cadena usuario;
-  vector_s amigos;
-  lista_s mensajes;
+typedef struct usuario
+{
+	int id;
+	t_cadena nombre;
+	t_cadena usuario;
+	vector_s amigos;
+	lista_s mensajes;
 }usuario;
 
 
@@ -49,8 +52,8 @@ typedef struct lista_usuarios;
 
 struct lista_usuarios
 {
-  lista_usuarios *sig;
-  usuario *datos;
+	lista_usuarios *sig;
+	usuario *datos;
 };
 
 
